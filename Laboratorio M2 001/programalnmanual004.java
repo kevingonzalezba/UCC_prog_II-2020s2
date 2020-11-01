888bd690362a7206f06a232f2943ca61c61529d5
Kevin Gonzalez <kevin.gonzalezba@campusucc.edu.co>
2020-10-31 20:40:11 -0500

    programacion en java
    Operadores Lógicos y
    Aritméticos

---
+++ Laboratorio M2 001/programalnmanual004.java
@@ -0,0 +1,50 @@
+package co.edu.campusucc;
+
+import java.util.Scanner;
+
+public class programalnmanual003 {
+	public static void main(String[] args) {
+		System.out.println("autor: Kevin Andres Gonzalez");
+		System.out.println("fecha: 31/OCTUBRE/2020");
+		System.out.println("____*##########*");
+		System.out.println("__*##############");
+		System.out.println("__################");
+		System.out.println("_##################_________*####*");
+		System.out.println("__##################_____*##########");
+		System.out.println("__##################___*#############");
+		System.out.println("___#################*_###############*");
+		System.out.println("____#################################*");
+		System.out.println("______###############################");
+		System.out.println("_______#############################");
+		System.out.println("________=##########################");
+		System.out.println("__________########################");
+		System.out.println("___________*#####################");
+		System.out.println("____________*##################");
+		System.out.println("_____________*###############");
+		System.out.println("_______________#############");
+		System.out.println("________________##########");
+		System.out.println("________________=#######*");
+		System.out.println("_________________######");
+		System.out.println("__________________####");
+		System.out.println("__________________###");
+		System.out.println("___________________#");
+		System.out.println("mi primer programa en java Kevin Gonzalez");
+		System.out.println("autor: Kevin Andres Gonzalez");
+        int rest1, rest2 = 1;
+		
+		boolean l1 = false, l2 =  false, k;
+		
+		try (Scanner leer = new Scanner(System.in)) {
+			System.out.println("¿inglaterra es un pais suramericano? [0]... falso [1]...verdad");
+			rest1 = leer.nextInt();
+				System.out.println("\"¿marie currie invento el radio? [0]... falso [1]...verdad ");
+				rest2 = leer.nextInt();
+		}
+		
+		if (rest1!= 0) l1=true;
+		if (rest2!= 0) l2=true;
+		
+		k= l1 && l2;
+		System.out.println("el valor final de k es:" +k);
+	}
+}
